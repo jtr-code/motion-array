dcc66b6b61eaf7bf2aa3c6dd6fcce81374ed793b
jtr-code <jtrtricks@gmail.com>
2024-12-10 17:33:42 +0530

    feat: configure server and middleware (CORS, cookie-parser, helmet)

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,21 @@
-console.log("hello index")
+import dotnev from "dotenv";
+
+import { app } from "./app.js";
+import { connectDb } from "./db/dbConfig.js";
+
+dotnev.config({
+  path: "./.env",
+});
+
+connectDb()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("⚙️  Server is running on port: " + process.env.PORT);
+    })
+  })
+  .catch((err) => {
+    console.error("Mongo db connection error: ", err)
+  })
+
+
+
